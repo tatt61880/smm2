6a8f89df27238fe3882b44990cd9dc829a8a8483
tatt61880 <tatt61880@gmail.com>
2024-07-16 00:30:35 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,12 +15,19 @@
     let result = '';
     const re = /\b([A-HJ-NP-Y0-9]{3})-?([A-HJ-NP-Y0-9]{3})-?([A-HJ-NP-Y0-9]{3})\b/gi;
 
+    const codeInInputStrMap = new Map();
     const levelCodeSet = new Set();
     const levelCodes = []; 
 
     let m;
     while (m = re.exec(inputText)) {
       const levelCode = `${m[1].toUpperCase()}-${m[2].toUpperCase()}-${m[3].toUpperCase()}`;
+      const codeInInputStr = m[0];
+
+      if (!codeInInputStrMap.has(levelCode) || codeInInputStr === levelCode) {
+        codeInInputStrMap.set(levelCode, codeInInputStr);
+      }
+
       if (!levelCodeSet.has(levelCode)) {
         levelCodeSet.add(levelCode);
         levelCodes.push(levelCode);
@@ -28,7 +35,12 @@
     }
 
     for (const levelCode of levelCodes) {
-      result += `<a target="_blank" href="https://smm2.wizul.us/smm2/level/${levelCode}">${levelCode}</a><br>`;
+      const codeInInputStr = codeInInputStrMap.get(levelCode);
+      let codeInfo = '';
+      if (codeInInputStr !== levelCode) {
+        codeInfo = ` (${codeInInputStr})`;
+      }
+      result += `<a target="_blank" href="https://smm2.wizul.us/smm2/level/${levelCode}">${levelCode}</a>${codeInfo}<br>`;
     }
 
     document.getElementById('result').innerHTML = result;
