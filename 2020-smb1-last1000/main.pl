c6f2bca4095cde9edebe82c0adce10db7b92c02f
tatt61880 <tatt61880@gmail.com>
2024-07-20 23:28:23 +0900

    add 2020-smb1-last1000

---
+++ 2020-smb1-last1000/main.pl
@@ -0,0 +1,110 @@
+use strict;
+use warnings;
+use utf8;
+
+my $userName;
+my $levelId;
+my %levelsUser;
+my %levelsNum;
+
+{
+    my $input = "input\\discord-log.txt";
+    open F, "<$input" or die;
+    binmode F, ":utf8";
+
+    while(<F>) {
+        chomp;
+
+        if (m/^(.*?) has cleared ".* \((...-...-...)\)! The clear rate is now \d+\/(\d+)+\. This is their (\d+)/) {
+            $userName = $1;
+            $levelId = $2;
+        }
+
+        if (m/^There are (\d*),?(\d+) uncleared 2020 courses!/) {
+            my $num = $1 . $2;
+            $levelsUser{$levelId} = $userName;
+            $levelsNum{$levelId} = $num;
+        }
+    }
+
+    close F;
+}
+
+{
+    my $output = "output\\temp.txt";
+    open FOUT, ">$output" or die;
+    binmode FOUT, ":utf8";
+
+    for my $id (sort {$levelsNum{$a} <=> $levelsNum{$b}} keys %levelsNum) {
+        my $levelId = $id;
+        my $user = $levelsUser{$levelId};
+        my $num = $levelsNum{$levelId};
+
+        print FOUT "$levelId\t$num\t$user\n";
+    }
+
+    close FOUT;
+}
+
+my %userNum;
+my @unclearedLevels;
+
+{
+    my $levels = "input\\last-1000-levels.txt";
+    open FIN, "<$levels" or die;
+    binmode FIN, ":utf8";
+
+    while(<FIN>) {
+        chomp;
+        last if (m/^$/);
+
+        m/^(.*)$/;
+        my $levelId = $1;
+        my $userName = $levelsUser{$levelId};
+
+        unless (defined $userName) {
+            push @unclearedLevels, $levelId;
+            next;
+        }
+
+        $userNum{$userName}++;
+    }
+
+    close FIN;
+}
+
+my $unclearedNum = 1000;
+
+{
+    my $output = "output\\ranking.txt";
+    open FOUT, ">$output" or die;
+    binmode FOUT, ":utf8";
+
+    print FOUT "Contributor ranking for last 1000 levels of 2020 SMB1.\n";
+    print FOUT "```\n";
+
+    for my $userName (sort {$userNum{$b} <=> $userNum{$a}} keys %userNum) {
+        my $num = $userNum{$userName};
+        $unclearedNum -= $num;
+
+        print FOUT "$userName: $num\n";
+    }
+
+    print FOUT "```\n";
+    print FOUT "Current uncleared levels: $unclearedNum\n";
+
+    close FOUT;
+}
+
+
+{
+    my $output = "output\\uncleared-levels.txt";
+    open FOUT, ">$output" or die;
+    binmode FOUT, ":utf8";
+
+    for my $levelId (@unclearedLevels) {
+        print FOUT "$levelId\n";
+    }
+
+    close FOUT;
+}
