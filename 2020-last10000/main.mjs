552629a2441d51e3386d72fe7f8ede438ce5954f
tatt61880 <tatt61880@gmail.com>
2024-08-24 22:11:45 +0900

    .

--- 2020-last10000/main.mjs
+++ 2020-last10000/main.mjs
@@ -3,6 +3,7 @@ import fs from 'fs';
 const idsText = fs.readFileSync(`./input/level-ids.txt`, 'utf-8');
 const ids = idsText.split(/\r?\n/).filter((line) => /^\w{3}-\w{3}-\w{3}$/.test(line));
 
+let count = 0;
 for (let id of ids) {
   const id_ = id.replaceAll('-', '');
   const filename = `./json/${id_}.json`;
@@ -10,12 +11,15 @@ for (let id of ids) {
   try {
     const jsonText = fs.readFileSync(filename, 'utf-8');
     const json = JSON.parse(jsonText);
-    const clears = json.clears;
+    const time = json.upload_time;
 
-    if (clears !== 0) {
+    if (time < 20000) {
       console.log(id);
+      count++;
     }
   } catch (err) {
     console.error(`Error: ${err}`);
   }
 }
+
+console.log(`count = ${count}`);
