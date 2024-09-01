17dcd943897e8c35c29bffb95b62e8f5f086950c
tatt61880 <tatt61880@gmail.com>
2024-09-02 02:51:51 +0900

    level / maker

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  const version = 'Version: 2024.08.31';
+  const version = 'Version: 2024.09.02';
 
   window.onload = function () {
     document.getElementById('version-info').innerText = version;
@@ -16,36 +16,80 @@
     const re = /\b([B-DF-HJ-NP-TV-Y0-9]{3})-?([B-DF-HJ-NP-TV-Y0-9]{3})-?([B-DF-HJ-NP-TV-Y0-9]{3})\b/gi;
 
     const codeInInputStrMap = new Map();
-    const levelCodeSet = new Set();
-    const levelCodes = [];
+    const codeSet = new Set();
+    const codes = [];
+    const codeInfoMap = new Map();
 
     while (true) {
       const m = re.exec(inputText);
       if (!m) break;
 
-      const levelCode = `${m[1].toUpperCase()}-${m[2].toUpperCase()}-${m[3].toUpperCase()}`;
+      const code = `${m[1].toUpperCase()}${m[2].toUpperCase()}${m[3].toUpperCase()}`;
+      const codePretty = `${m[1].toUpperCase()}-${m[2].toUpperCase()}-${m[3].toUpperCase()}`;
       const codeInInputStr = m[0];
 
-      if (!codeInInputStrMap.has(levelCode) || codeInInputStr === levelCode) {
-        codeInInputStrMap.set(levelCode, codeInInputStr);
+      if (!codeInInputStrMap.has(codePretty) || codeInInputStr === codePretty) {
+        codeInInputStrMap.set(codePretty, codeInInputStr);
       }
 
-      if (!levelCodeSet.has(levelCode)) {
-        levelCodeSet.add(levelCode);
-        levelCodes.push(levelCode);
+      if (!codeSet.has(codePretty)) {
+        codeSet.add(codePretty);
+        codes.push(codePretty);
+        const codeInfo = getCodeInfo(code);
+        codeInfoMap.set(codePretty, codeInfo);
       }
     }
 
-    for (const levelCode of levelCodes) {
-      const codeInInputStr = codeInInputStrMap.get(levelCode);
-      let codeInfo = '';
-      if (codeInInputStr !== levelCode) {
-        codeInfo = ` (${codeInInputStr})`;
+    for (const code of codes) {
+      const codeInInputStr = codeInInputStrMap.get(code);
+      let textInfo = '';
+      if (codeInInputStr !== code) {
+        textInfo = ` (${codeInInputStr})`;
       }
-      const viewerUrl = `https://smm2.wizul.us/smm2/level/${levelCode}`;
-      result += `<span class="level-code">${levelCode}: <a target="_blank" href="${viewerUrl}">${viewerUrl}</a>${codeInfo}</span><br>`;
+      const codeInfo = codeInfoMap.get(code);
+      const type = codeInfo.d === 1n ? 'maker' : 'level';
+      const viewerUrl = `https://smm2.wizul.us/smm2/${type}/${code}`;
+      result += `<span class="level-code">${code}: <a target="_blank" href="${viewerUrl}">${viewerUrl}</a>${textInfo}</span><br>`;
     }
 
     document.getElementById('result').innerHTML = result;
+  }
+
+  function getCodeInfo(code) {
+    const cToVal = {
+      '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+      '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+      'B': 10, 'C': 11, 'D': 12, 'F': 13, 'G': 14,
+      'H': 15, 'J': 16, 'K': 17, 'L': 18, 'M': 19,
+      'N': 20, 'P': 21, 'Q': 22, 'R': 23, 'S': 24,
+      'T': 25, 'V': 26, 'W': 27, 'X': 28, 'Y': 29
+    };
+
+    let num = 0;
+    for (const c of code.split('').reverse()) {
+      num *= 30;
+      num += cToVal[c];
+    }
+
+    const lengths = [12n, 1n, 1n, 20n, 6n, 4n];
+    const vals = [];
+    let numBigInt = BigInt(num);
+    for (const length of lengths) {
+      const mask = (1n << length) - 1n;
+      const val = numBigInt & mask;
+      vals.unshift(val);
+      numBigInt >>= length;
+    }
+
+    const a = vals[0];
+    const b = vals[1];
+    const c = vals[2];
+    const d = vals[3];
+    const e = vals[4];
+    const f = vals[5];
+
+    const n = 0x1680E07Cn ^ (c | (f << 20n));
+
+    return { a, b, c, d, e, f, n };
   }
 })();
